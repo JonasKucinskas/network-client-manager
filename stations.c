3b6d6f5d5dc5acd4a12e64292faea96fb34bf994
JonasKucinskas <jonaskucinskas2@gmail.com>
2025-02-13 12:27:00 +0200

    multi-client support

--- stations.c
+++ stations.c
@@ -10,16 +10,15 @@ static int get_client_count(GtkTextBuffer *buffer)
   return line_count / LINE_COUNT_PER_CLIENT;
 }
 
-static char* get_client_macs(GtkTextBuffer *buffer, int *client_count)
+static gchar** get_client_macs(GtkTextBuffer *buffer, int client_count)
 {
   GtkTextIter start_iter;
   GtkTextIter end_iter;
 
-  //17 chars in a mac address
-  char *macs = malloc(17 * (*client_count) * sizeof(char));
+  gchar** macs = g_new(gchar*, client_count);
   gint line;
 
-  for (int i = 0; i < (*client_count); i++)
+  for (int i = 0; i < client_count; i++)
   {
     line = i * LINE_COUNT_PER_CLIENT;
 
@@ -32,20 +31,16 @@ static char* get_client_macs(GtkTextBuffer *buffer, int *client_count)
 
   return macs;
 }
-/*
-static void dc_client(GtkButton *button, GtkTextBuffer *buffer)
-{ 
-  const gchar *mac = get_client_mac(buffer);
 
+static void dc_client(GtkButton *button, gchar *mac)
+{ 
   char cmd[47] = "sudo iw dev wlan0 station del ";
   strcat(cmd, mac);
 
   FILE *fp = popen(cmd, "r");
   fclose(fp);
 }
-*/
-//iw dev wlan0 station get <MAC address>
-//iw dev wlan0 station del ca:1c:72:9e:79:d2
+
 static gboolean set_input(GtkTextBuffer *buffer)
 {
   char *cmd = "iw dev wlan0 station dump";
@@ -53,18 +48,10 @@ static gboolean set_input(GtkTextBuffer *buffer)
   FILE* fp;
 
   GtkTextIter end;
-  GtkTextIter start;
-
-  gtk_text_buffer_get_start_iter(buffer, &start);
   gtk_text_buffer_get_end_iter(buffer, &end);
-  gtk_text_buffer_delete(buffer, &start, &end);
+
 
   fp = popen(cmd, "r");
-  if (fp == NULL)
-  {
-    //print error;
-  }
-
   while (fgets(buf, BUFSIZE, fp) != NULL)
   {
     gtk_text_buffer_insert(buffer, &end, buf, -1);
@@ -72,6 +59,40 @@ static gboolean set_input(GtkTextBuffer *buffer)
 
   fclose(fp);
   return TRUE;
+}
+
+static void set_buffers(GtkTextBuffer** buffers, gchar** macs, int client_count)
+{ 
+  char cmd[] = "iw dev wlan0 station get ";
+  char cmd_full[43] = {0};
+
+  for (int i = 0; i < client_count; i++)
+  {
+    snprintf(cmd_full, sizeof(cmd_full), "%s%s", cmd, macs[i]);
+
+    char buf[BUFSIZE] = {0};
+    FILE* fp;
+
+    buffers[i] = gtk_text_buffer_new(NULL);
+
+    //needed for updates
+    GtkTextIter end;
+    GtkTextIter start;
+
+    gtk_text_buffer_get_start_iter(buffers[i], &start);
+    gtk_text_buffer_get_end_iter(buffers[i], &end);
+    gtk_text_buffer_delete(buffers[i], &start, &end);
+    //
+
+    fp = popen(cmd_full, "r");
+
+    while (fgets(buf, BUFSIZE, fp) != NULL)
+    {
+      gtk_text_buffer_insert(buffers[i], &end, buf, -1);
+    }
+
+    fclose(fp);
+  }
 }
 
 static void activate (GtkApplication *app, gpointer user_data)
@@ -92,42 +113,33 @@ static void activate (GtkApplication *app, gpointer user_data)
   //initial buffer load
   set_input(buffer);
 
-  //get client count
-  //pasiimti kiekvieno cliento mac,
-  //kiekvieno cliento mac paleisti 
-  //iw dev wlan0 station get <MAC address>.
-  //ideti kiekviena i nauja bufferi.
-
-
-
-  int *client_count;
+  int client_count = get_client_count(buffer);
   //g_timeout_add(1000, (GSourceFunc)get_client_count, buffer, &client_count);
-  client_count = get_client_count(buffer);
   
-
   //refresh client info
   //g_timeout_add(1000, (GSourceFunc)set_input, buffer);
 
-  char *macs = get_client_macs(buffer, client_count);
+  GtkTextBuffer **buffers = g_new(GtkTextBuffer*, client_count);
+  gchar **macs = get_client_macs(buffer, client_count);
+
+  set_buffers(buffers, macs, client_count);
 
   for (int i = 0; i < client_count; i++)
   {
-    g_print(macs[i]);
+    text_view = gtk_text_view_new_with_buffer(buffers[i]);
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
+    
+    //2 horizontaly, 1 verticaly
+    gtk_grid_attach(GTK_GRID(grid), text_view, 0, i * 2, 2, 1);
+    
+    
+    //set button
+    //column 0, row 1, span 2 horizontaly, 1 verticaly
+    GtkWidget *dc_button = gtk_button_new_with_label("Disconnect");
+    g_signal_connect(dc_button, "clicked", G_CALLBACK (dc_client), macs[i]);
+    gtk_grid_attach(GTK_GRID(grid), dc_button, 0, i * 2 + 1, 2, 1);
+
   }
-
-
-  text_view = gtk_text_view_new_with_buffer(buffer);
-  gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
-
-  gtk_grid_attach(GTK_GRID(grid), text_view, 0, 0, 2, 1);
-
-  //gtk_container_add(GTK_CONTAINER (grid), text_view);
-  
-  //set buttons
-  //column 0, row 1, span 2 horizontaly, 1 verticaly
-  GtkWidget *dc_button = gtk_button_new_with_label("Disconnect");
-  //g_signal_connect(dc_button, "clicked", G_CALLBACK (dc_client), buffer);
-  gtk_grid_attach(GTK_GRID(grid), dc_button, 0, 1, 2, 1);
 
   gtk_widget_show_all (window);
 }
